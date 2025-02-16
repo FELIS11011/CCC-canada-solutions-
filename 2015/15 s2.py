635f87f750ff62b8cd5cb02b617c71dff3bd68d3
FELIS <dhy090617@gmail.com>
2025-02-15 20:25:50 -0500

    Update 15 s2.py

--- 2015/15 s2.py
+++ 2015/15 s2.py
@@ -1,1 +1,28 @@
-aaaa
+j = int (input ())
+a = int (input ())
+
+sizes = []
+statisfy = 0
+used = [False] * j
+
+
+for i in range (j) :
+    sizes.append (input ())
+
+for i in range (a) :
+    size = ""
+    number = 0 
+    size , number = input ().split ()
+    number = int (number) - 1
+    if used [number] == False and number < j  :
+        if size == "L" and sizes [number] == "L":
+            statisfy += 1 
+            used [number] = True 
+        elif size == "M" and (sizes [number] == "L" or sizes [number] == "M"):
+            statisfy += 1 
+            used [number] = True
+        elif size == "S" and (sizes [number] == "M" or sizes [number] == "L" or sizes [number] == "S"):
+            statisfy += 1 
+            used [number] = True
+
+print (statisfy)
