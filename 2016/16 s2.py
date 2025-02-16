a96b09351d94176f30a8af178c6428e158d8b74c
FELIS <dhy090617@gmail.com>
2025-02-15 23:20:50 -0500

    Update 16 s2.py

--- 2016/16 s2.py
+++ 2016/16 s2.py
@@ -1,1 +1,23 @@
-a
+q = int (input ())
+n = int (input ())
+
+city1 = []
+city2 = []
+
+city1 = input().split ()
+city2 = input ().split ()
+
+cities = city1 + city2
+
+cities = [int (x) for x in cities]
+
+cities.sort ()
+
+midpoint = len(cities)//2
+
+
+
+if q == 1:
+    print (sum (cities [: midpoint ]))
+if q == 2:
+    print (sum (cities [1::2]))
